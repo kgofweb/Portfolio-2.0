b82ba696ae93cd111bbacedc8f1ce617fc22f333
mervydevmedia <mervydevmedia@gmail.com>
2022-01-05 16:26:35 +0000

    Third day

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@
 const navMenu = document.getElementById('nav-menu')
 const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
-const navLink = document.querySelectorAll('.nav--link');
-
+const navLink = document.querySelectorAll('.nav--link')
+const skillsContent = document.getElementsByClassName('skills--content')
+const skillsHeader = document.querySelectorAll('.skills--header')
+// Qualifications
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
 
 // SHOW MENU
 navToggle.addEventListener('click', () => {
@@ -21,3 +25,35 @@ navLink.forEach(link => link.addEventListener('click', linkAction))
 function linkAction() {
   navMenu.classList.remove('show-menu')
 }
+
+// VIEWS SKILLS
+skillsHeader.forEach(el => el.addEventListener('click', toggleSkills))
+
+function toggleSkills() {
+  let itemsClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills--content skills--close'
+  }
+
+  if (itemsClass === 'skills--content skills--close') {
+    this.parentNode.className = 'skills--content skills--open'
+  }
+}
+
+// WORK WITH QUALIFICATION TABS
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+  
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification--active')
+    })
+    target.classList.add('qualification--active')
+  
+    tab.forEach(tab => {
+      tab.classList.remove('qualification--active')
+    })
+    tab.classList.add('qualification--active')
+  })
+})
